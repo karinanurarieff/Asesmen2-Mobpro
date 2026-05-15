84ba53de3e625e6c3653947bc01609cdc9155c57
karinanurarieff <karinaarieffin68138@gmail.com>
2026-05-15 11:06:00 +0700

    Create task entity

---
+++ app/src/main/java/com/karina0088/studenttasktracker/data/Task.kt
@@ -0,0 +1,13 @@
+package com.karina0088.studenttasktracker.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tasks")
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+    val title: String,
+    val subject: String,
+    val deadline: String
+)
