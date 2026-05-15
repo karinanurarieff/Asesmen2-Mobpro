91ffec8d340a693e1c303555b5d521fb61333975
karinanurarieff <karinaarieffin68138@gmail.com>
2026-05-15 11:18:24 +0700

    Setup navigation

--- app/src/main/java/com/karina0088/studenttasktracker/MainActivity.kt
+++ app/src/main/java/com/karina0088/studenttasktracker/MainActivity.kt
@@ -4,44 +4,43 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.karina0088.studenttasktracker.ui.AddEditScreen
+import com.karina0088.studenttasktracker.ui.HomeScreen
 import com.karina0088.studenttasktracker.ui.theme.StudentTaskTrackerTheme
+import com.karina0088.studenttasktracker.viewmodel.TaskViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
         setContent {
             StudentTaskTrackerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AppNav()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun AppNav() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    StudentTaskTrackerTheme {
-        Greeting("Android")
+    val navController = rememberNavController()
+    val viewModel: TaskViewModel = viewModel()
+
+    NavHost(navController = navController, startDestination = "home") {
+
+        composable("home") {
+            HomeScreen(navController, viewModel)
+        }
+
+        composable("add") {
+            AddEditScreen(navController, viewModel)
+        }
     }
 }
